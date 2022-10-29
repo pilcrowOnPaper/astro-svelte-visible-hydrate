1532c72f4c03a95f5c9ab15f2e7b25bcc95ab890
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2022-10-29 23:23:46 +0900

    initial commit

---
+++ src/components/ClientLoad.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let message = "not yet...";
+
+    onMount(() => {
+        console.log("client:load")
+        message = "yes!";
+    });
+</script>
+
+<link rel="stylesheet" href="./styles.css" />
+<h2>client:load</h2>
+<p>Hydrated?: {message}</p>
---
+++ src/components/ClientVisible.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let message = "not yet...";
+
+    onMount(() => {
+        console.log("client:visible")
+        message = "yes!";
+    });
+</script>
+
+<link rel="stylesheet" href="./styles.css" />
+<h2>client:visible</h2>
+<p>Hydrated?: {message}</p>
